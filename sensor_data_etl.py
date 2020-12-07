8a3c0e17527bdab0d1476392635c821ab1ad7a89
Babak <b.badkoubeh@gmail.com>
2020-12-07 11:49:00 -0700

    new repo

--- sensor_data_etl.py
+++ sensor_data_etl.py
@@ -1,7 +1,5 @@
-import operator
-import time, random, threading
 import sys
-import uuid
+
 from datetime import datetime, timedelta
 
 assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
@@ -14,117 +12,90 @@ sensor_config_table = "sensor_configs"
 sensor_data_table = "sensor_data"
 keyspace = "bba37"
 
-# SQL server configs
-server_name = "jdbc:sqlserver://reportingprod.database.windows.net"
-username = "trakadmin"
-password = "T@l@m@t!cs_10"
-connectionProperties = {
-    "user": username,
-    "password": password,
-    "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-}
-sql_db_sensor_data = "TrakopolisDataProd_0013_CopyLH"
-sql_db_sensor_configs = "TrakopolisProd_CopyLH"
-url_sensor_data = "{0}; databaseName= {1};".format(server_name, sql_db_sensor_data)
-url_sensor_configs = "{0}; databaseName= {1};".format(server_name, sql_db_sensor_configs)
-
 # spark = SparkSession.builder.master('local[*]').appName('SQL_Cassandra') \
 #     .config('spark.cassandra.connection.host', ','.join(cluster_seeds)) \
 #     .getOrCreate()
-spark = SparkSession.builder.master('local[*]').appName('SQL_polling').getOrCreate()
+spark = SparkSession.builder.appName('sensor data ETL').getOrCreate()
 assert spark.version >= '2.4'  # make sure we have Spark 2.4+
 sc = spark.sparkContext
 sc.setLogLevel('WARN')
 
-# Kafka configs
-kafka_topic = "asset_data"
-
-sensor_division_Id = 3550
-
-sql_gas_sensor_data = """(SELECT
-                            AssetData.TerminalID [terminal_id],
-                            Terminal.TerminalTypeId [terminal_type_id],
-                            Terminal.AssetID [asset_id],
-                            AssetData.[ID] [asset_data_id], 
-                            Asset.DivisionId [division_id],
-                            AssetData.[Created] [datetime],
-                            AssetData.[Latitude] [latitude], 
-                            AssetData.[Longitude] [longitude],
-                            AssetData.[MessageCodeID] [message_code_id],
-                            AnalogSensorData.[value] [sensor_reading],
-                            AnalogSensorData.[Sensor] [sensor_type_id],
-                            SiteInfo.GeofenceID [site_id],
-                            SiteInfo.IsEnter [site_transition_state],
-                            SiteInfo.DateTime [site_transition_datetime]
-                        FROM AssetData
-                        INNER JOIN AnalogSensorData on AssetData.ID = AnalogSensorData.AssetDataID
-                        LEFT JOIN SummaryGeofence SiteInfo ON SiteInfo.AssetDataID = AssetData.ID
-                        INNER JOIN Terminal ON AssetData.TerminalID = Terminal.ID
-                        INNER JOIN Asset ON Asset.ID = Terminal.AssetId
-                        WHERE Terminal.TerminalTypeID IN (58, 49)
-                            AND Asset.divisionId = {0}
-                            AND AssetData.MessageCodeId IN (10, 100, 64, 66, 67, 68, 69)
-                            AND AssetData.Created > '{1}'
-                            AND AssetData.Created < '{2}') RawDataPoints"""
-
-sql_sensor_config_query = """(SELECT
-                                   Terminal.ID [terminal_id],
-                                   Terminal.TerminalTypeId [terminal_type_id], 
-                                   tas.Sensor [sensor_type_id], 
-                                   tas.Name [sensor_name],
-                                   tas.Units [sensor_unit]
-                                 FROM Terminal
-                                 INNER JOIN TerminalAnalogSensors tas ON tas.TerminalID = Terminal.ID
-                                 WHERE Terminal.TerminalTypeId IN (58, 49)) Terminals"""
-
 
 def main():
-    sensor_config_df = spark.read.jdbc(url=url_sensor_configs,
-                                       table=sql_sensor_config_query,
-                                       numPartitions=3,
-                                       properties=connectionProperties)
+    sensor_config_df = spark.read.parquet('sensor_configs')
+    message_code_df = spark.read.parquet('message_code_table')
     sensor_config_df = sensor_config_df.select(sensor_config_df['terminal_id'],
                                                sensor_config_df['sensor_type_id'],
                                                sensor_config_df['sensor_name'],
-                                               sensor_config_df['sensor_unit']).cache()
+                                               sensor_config_df['sensor_unit']).repartition(50)
+    sensor_config_df = sensor_config_df.withColumn('sensor_name',
+                                                   functions.when(functions.col('sensor_name') == 'CO ', 'CO')
+                                                   .otherwise(functions.col('sensor_name')))
 
-    # now = datetime.utcnow()
-    now = datetime(2019, 3, 1)
-    start_date = datetime(2019, 1, 1)
-    interval = 10
-    sensor_data_count_total = 0
+    raw_data_schema = types.StructType([
+        types.StructField('terminal_id', types.LongType()),
+        types.StructField('terminal_type_id', types.IntegerType()),
+        types.StructField('asset_id', types.IntegerType()),
+        types.StructField('asset_data_id', types.LongType()),
+        types.StructField('division_id', types.IntegerType()),
+        types.StructField('datetime', types.TimestampType()),
+        types.StructField('latitude', types.FloatType()),
+        types.StructField('longitude', types.FloatType()),
+        types.StructField('message_code_id', types.IntegerType()),
+        types.StructField('sensor_reading', types.FloatType()),
+        types.StructField('sensor_type_id', types.IntegerType()),
+        types.StructField('site_id', types.IntegerType()),
+        types.StructField('site_transition_state', types.BooleanType()),
+        types.StructField('site_transition_datetime', types.TimestampType())])
 
-    sensor_data_schema = types.StructType([])
-    sensor_data_df = spark.createDataFrame(sc.emptyRDD(), schema=sensor_data_schema)
+    new_data_points_df = spark.read.csv('sensor_raw_data', schema=raw_data_schema).repartition(200)
 
-    # while start_date < now:
-    end_date = start_date + timedelta(days=interval)
-    new_data_points_df = spark.read.jdbc(url=url_sensor_data,
-                                         table=sql_gas_sensor_data.format(sensor_division_Id,
-                                                                          start_date.strftime("%Y-%m-%d"),
-                                                                          end_date.strftime("%Y-%m-%d")),
-                                         numPartitions=6,
-                                         properties=connectionProperties)
-
-    sensor_data_count = new_data_points_df.count()
-    print("Polled {0} data points from {1} to {2}".format(sensor_data_count,
-                                                          start_date.strftime("%Y-%m-%d"),
-                                                          end_date.strftime("%Y-%m-%d")))
-    sensor_data_count_total += sensor_data_count
-    start_date = end_date
+    # Filter data for testing
+    # new_data_points_df = new_data_points_df.where(new_data_points_df['datetime'] < datetime(2019, 1, 6))
 
     sensor_data = new_data_points_df.join(sensor_config_df,
                                           (new_data_points_df['terminal_id'] == sensor_config_df['terminal_id'])
                                           & (new_data_points_df['sensor_type_id'] == sensor_config_df[
                                               'sensor_type_id']))
+    print("No. Records after join operation: ", sensor_data.count())
+    drop_extra_cols = sensor_data.select(sensor_data['asset_data_id'],
+                                         sensor_data['datetime'],
+                                         sensor_data['latitude'],
+                                         sensor_data['longitude'],
+                                         sensor_data['message_code_id'],
+                                         sensor_data['sensor_name'],
+                                         sensor_data['sensor_reading']).orderBy(sensor_data['asset_data_id'])
 
-    sensor_reads_df = sensor_data_df.groupBy(sensor_data_df['asset_data_id'])\
-        .agg(functions.collect_set())
+    target_df = drop_extra_cols.groupBy(drop_extra_cols['datetime'],
+                                        drop_extra_cols['latitude'],
+                                        drop_extra_cols['longitude']) \
+        .agg(functions.max(drop_extra_cols['message_code_id']).alias('message_code_id'))
 
-    sensor_data.write.format("org.apache.spark.sql.cassandra") \
-        .mode('append') \
-        .options(**{'confirm.truncate': True}) \
-        .options(table='sensor_data', keyspace="bba37").save()
+    features_df = drop_extra_cols.groupBy(drop_extra_cols['datetime'],
+                                          drop_extra_cols['latitude'],
+                                          drop_extra_cols['longitude']).pivot('sensor_name') \
+        .agg(functions.first(drop_extra_cols['sensor_reading'], ignorenulls=True))
+
+    features_df = features_df.fillna(0, subset=['CO', 'LEL', 'H2S'])
+    features_df = features_df.fillna(21, subset=['O2'])
+
+    output = features_df.join(target_df, ['datetime', 'latitude', 'longitude'])
+
+    output = output.join(functions.broadcast(message_code_df), 'message_code_id')
+
+    # # dropping some columns
+    output = output.drop('Battery level')
+    output = output.drop('Gas Concentration #6')
+    # output = output.withColumnRenamed('Battery level', 'Battery_level') \
+    #     .withColumnRenamed('Gas Concentration #6', 'Gas_concentration_6')
+    print("Total records: ", output.count())
+    # output.where(output['message_code_id'].isin([66, 67, 68, 69, 100])).show(30)
+    output.write.parquet('sensor_data_ts')
+    # sensor_data.write.format("org.apache.spark.sql.cassandra") \
+    #     .mode('append') \
+    #     .options(**{'confirm.truncate': True}) \
+    #     .options(table='sensor_data', keyspace="bba37").save()
+
 
 if __name__ == "__main__":
     main()
